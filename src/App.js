8b4732dd2c690f6b58ef6298e124a7e28640eadc
Vitoria Pacheco <vitoriac.pac@gmail.com>
2021-09-13 12:09:19 -0300

    Estilos e estrutura

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="valid-keys">
+        <span className="matched">Vito</span>
+        <span className="remaider">ria</span>
+      </div>
+      <div className="typed-keys">asdfvitoasdf</div>
+      <div className="completed-words">
+        <ol>
+          <li>Cidade</li>
+          <li>Casa</li>
+          <li>Logo</li>
+        </ol>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
